ca2c1eee147cf4abd48e9574f92cb95c28016a2f
Chris Marchesi <chrism@vancluevertech.com>
2017-06-04 20:10:35 -0700

    Switch ID generation to UUID, add collision error type
    Rather than using the 64-byte length random ID that we were using (32
    bytes represented by hex), switched IDs to be standard v4 random UUIDs.
    This still offers a near impossible risk of collisions, with the sheer
    amount of records required to approach any iota of a probability of
    hitting one being so high that you are probably going to hit other
    scalability issues with this store before that. Way before that.
    
    Nonetheless, also added a IDCollisionError type so that one can check
    for collisions via a type switch, if it's ever necessary.

--- pub/pub.go
+++ pub/pub.go
@@ -3,7 +3,6 @@
 package pub
 
 import (
-	"crypto/rand"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,7 +10,28 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+
+	"github.com/google/uuid"
 )
+
+// IDCollisionError is an error type that is returned on a UUID collision.
+// This is a retryable error.
+//
+// While this error exists and you can check for it, the changes of it
+// happening are extremely rare, as the uuid package so explains:
+//
+//   Randomly generated UUIDs have 122 random bits.  One's annual risk of being
+//   hit by a meteorite is estimated to be one chance in 17 billion, that means
+//   the probability is about 0.00000000006 (6 × 10−11), equivalent to the odds
+//   of creating a few tens of trillions of UUIDs in a year and having one
+//   duplicate.
+type IDCollisionError struct {
+	s string
+}
+
+func (e *IDCollisionError) Error() string {
+	return e.s
+}
 
 // Publisher is a simple event publisher, designed to publish events to the
 // file system.
@@ -59,8 +79,7 @@ func NewPublisher(dir string, event interface{}) (*Publisher, error) {
 }
 
 // Publish publishes an event. The event is a single file in the directory,
-// with a random 32-byte hex as the ID and filename. The ID is returned as a
-// string.
+// with a v4 UUID as the ID and filename. The ID is returned as a string.
 func (p *Publisher) Publish(event interface{}) (string, error) {
 	if reflect.TypeOf(event) != p.eventType {
 		return "", fmt.Errorf("event of type %s does not match publisher type %s", reflect.TypeOf(event), p.eventType)
@@ -69,17 +88,24 @@ func (p *Publisher) Publish(event interface{}) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("could not marshal event data: %s", err)
 	}
-	idb := make([]byte, 32)
-	if _, err := rand.Read(idb); err != nil {
+	id, err := uuid.NewRandom()
+	if err != nil {
 		return "", fmt.Errorf("could not generate ID: %s", err)
 	}
-	id := fmt.Sprintf("%x", idb)
 
-	path := p.dir + "/" + id
+	path := p.dir + "/" + id.String()
+	// If this path responds to stat, then the path exists in some way, shape, or
+	// form, and is not valid for use. This is almost always due to a UUID
+	// collision, so return IDCollisionError. If the stat failed and it is due to
+	// some other error other than the file being missing, it will be caught when
+	// we write to the file.
+	if _, err := os.Stat(path); err == nil {
+		return "", &IDCollisionError{s: fmt.Sprintf("id collision: %s", id)}
+	}
 
 	if err := ioutil.WriteFile(path, data, 0666); err != nil {
 		return "", fmt.Errorf("error writing event to file %s: %s", path, err)
 	}
 
-	return id, nil
+	return id.String(), nil
 }
